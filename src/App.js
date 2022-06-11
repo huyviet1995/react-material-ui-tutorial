ebea7770e758a1e78e1299dda6b43d0e9b6330d0
Viet <viet.dang@dirox.net>
2022-06-12 00:43:53 +0700

    Adding very basic layout for TouCard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import "./App.css";
+import TourCard from "./components/TourCard";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Container>
+        <Grid container spacing={5}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,13 @@
+import { Paper } from "@mui/material";
+
+const TourCard = () => {
+    return (
+        <Grid item>
+            <Paper elevation={3}>
+                Hello
+            </Paper>
+        </Grid>
+    ) 
+}
+
+export default TourCard;
