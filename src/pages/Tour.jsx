e9438c2436e2013d9f24ed26d398efb25f7b90ea
Viet <viet.dang@dirox.net>
2022-06-12 20:31:39 +0700

    Showing the date picker and I click the Open Modal button

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,5 +1,8 @@
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Paper, BottomNavigation } from "@mui/material";
+import CustomizedAccordians from "../components/Accordian";
+import StaticDateRangePickerDemo from "../components/DateRangePicker";
 import ImageCollage from '../components/ImageCollage'
+import BasicModal from "../components/Modal";
 
 const Tour = () => {
     return (
@@ -11,6 +14,27 @@ const Tour = () => {
                 <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="breakfast" height={325} />
                 <ImageCollage></ImageCollage>
             </Box>
+            <Box>
+                <Typography variant="h6" component="h4" marginTop={3}>
+                    About this ticket
+                </Typography>
+                <Typography variant="paragraph" component="p">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus praesentium quod aspernatur exercitationem nihil dolorum ratione alias, reiciendis, aliquid unde corporis, reprehenderit eum maiores cupiditate autem earum illum aut. Neque.
+                </Typography>
+            </Box>
+            <Box marginBottom={10}>
+                <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+                    Frequently asked questions
+                </Typography>
+                <CustomizedAccordians />
+            </Box>
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+                <BottomNavigation showLabels>
+                    <BasicModal>
+                        <StaticDateRangePickerDemo />
+                    </BasicModal>
+                </BottomNavigation>
+            </Paper>
         </Container>
     )   
 }
