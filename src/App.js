fdf5b44515ae544814e39d52504c118e1af896b3
Viet <viet.dang@dirox.net>
2022-06-12 11:36:22 +0700

    Using data.json files to populate the files data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import "./App.css";
 import SearchAppBar from "./components/Appbar";
 import TourCard from "./components/TourCard";
+import cities from './data.json';
 
 function App() {
   return (
@@ -10,10 +11,27 @@ function App() {
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
+          {cities.map((city, index) => (
+            <div key={`${city.name}_${index}`}>
+              <Typography 
+                variant="h4" 
+                component="h2" 
+                marginTop={5} 
+                marginBottom={3}
+              >
+                Top {city.name} Tours
+                <Grid container spacing={5}>
+                  {city.tours.map((tour, index) => (
+                    <TourCard tour={tour} key={`${tour.name}_${index}`}/>
+                  ))}
+                </Grid>
+              </Typography>
+            </div>
+          ))}
+          {/* <TourCard />
           <TourCard />
           <TourCard />
-          <TourCard />
-          <TourCard />
+          <TourCard /> */}
         </Grid>
       </Container>
     </div>
