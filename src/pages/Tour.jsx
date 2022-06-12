f4cb2c325547fa05137ed46e654e33f5b124f08d
Viet <viet.dang@dirox.net>
2022-06-12 14:19:03 +0700

    Use ImageList from the Material UI

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,11 +1,16 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
+import ImageCollage from '../components/ImageCollage'
 
 const Tour = () => {
     return (
         <Container sx={{ width: 800 }}>
-            <Typography>
-                Explore the world
+            <Typography variant="h3" component="h1">
+                Explore the World in Vegas
             </Typography>
+            <Box marginTop={3} sx={{ display: 'flex', width: '100%' }}>
+                <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="breakfast" height={325} />
+                <ImageCollage></ImageCollage>
+            </Box>
         </Container>
     )   
 }
