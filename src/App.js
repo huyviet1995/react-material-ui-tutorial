16aeabecc9b28b216ed2fd63584cf72ae3037273
Viet <viet.dang@dirox.net>
2022-06-12 10:45:28 +0700

    Adding navabar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import "./App.css";
+import SearchAppBar from "./components/Appbar";
 import TourCard from "./components/TourCard";
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
           <TourCard />
           <TourCard />
