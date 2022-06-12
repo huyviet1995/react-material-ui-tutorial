72291e2b5633444fb025d638b4ade073ed0cccc5
Viet <viet.dang@dirox.net>
2022-06-12 13:16:16 +0700

    Remove the redundant Grid Container

--- src/App.js
+++ src/App.js
@@ -3,39 +3,33 @@ import { Container } from "@mui/system";
 import "./App.css";
 import SearchAppBar from "./components/Appbar";
 import TourCard from "./components/TourCard";
-import cities from './data.json';
+import cities from "./data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
-          {cities.map((city, index) => (
-            <div key={`${city.name}_${index}`}>
-              <Typography 
-                variant="h4" 
-                component="h2" 
-                marginTop={5} 
-                marginBottom={3}
-              >
-                Top {city.name} Tours
-              </Typography>
-                <Grid container spacing={5}>
-                  {city.tours.map((tour, index) => (
-                    <TourCard tour={tour} key={`${tour.name}_${index}`}/>
-                  ))}
-                </Grid>
-            </div>
-          ))}
-          {/* <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard /> */}
-        </Grid>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SearchAppBar />
+            <Container sx={{ marginY: 5 }}>
+                {cities.map((city, index) => (
+                    <div key={`${city.name}_${index}`}>
+                        <Typography
+                            variant="h4"
+                            component="h2"
+                            marginTop={5}
+                            marginBottom={3}
+                        >
+                            Top {city.name} Tours
+                        </Typography>
+                        <Grid container spacing={5}>
+                            {city.tours.map((tour, index) => (
+                                <TourCard tour={tour} key={`${tour.name}_${index}`} />
+                            ))}
+                        </Grid>
+                    </div>
+                ))}
+            </Container>
+        </div>
+    );
 }
 
 export default App;
